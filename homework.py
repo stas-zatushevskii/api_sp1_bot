f3659968e6161f98106e7847aeffe2e6b33bcb5a
stas-zatushevskii <86684411+stas-zatushevskii@users.noreply.github.com>
2021-09-23 22:01:09 +0300

    a

--- homework.py
+++ homework.py
@@ -1,15 +1,15 @@
 import logging
+from logging.handlers import RotatingFileHandler
 import os
 import time
-from logging.handlers import RotatingFileHandler
 
-import requests
+
 from dotenv import load_dotenv
+import requests
 from telegram import Bot
 
 load_dotenv()
 
-logging.basicConfig(level=logging.INFO)
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
@@ -17,41 +17,50 @@ URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 
 bot = Bot(token=TELEGRAM_TOKEN)
 
-logging.basicConfig(
-    level=logging.INFO,
-    filename='main.log',
-    filemode='w',
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-)
+if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.INFO,
+        filename='main.log',
+        filemode='w',
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+
+    ), os.path.expanduser('~/main.log')
+
 
 logger = logging.getLogger(__name__)
 handler = RotatingFileHandler(
     'my_logger.log', maxBytes=50000000, backupCount=5
 )
-logger.addHandler(handler)
+
+PHRASE_FOR_rejected = 'К сожалению, в работе нашлись ошибки.'
+PHRASE_FOR_reviewing = 'Работа взята в ревью'
+PHRASE_FOR_approved = 'Ревьюеру всё понравилось, работа зачтена!'
+PHRASE_FOR_otvet = (
+    'У вас проверили работу "{name_to_insert}" !\n\n{verdict_to_insert}')
 
 
 def parse_homework_status(homework):
-    homework_name = homework.get('homework_name')
-    try:
-        if homework.get('status') == 'rejected':
-            verdict = 'К сожалению, в работе нашлись ошибки.'
-        else:
-            verdict = 'Ревьюеру всё понравилось, работа зачтена!'
-        return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
-    except Exception as error:
-        print(f'Чет не то: {error}')
-        logging.error(error, exc_info=True)
+    homework_name = homework['homework_name']
+    if homework['status'] == 'rejected':
+        verdict = PHRASE_FOR_rejected
+    elif homework['status'] == 'reviewing':
+        verdict = PHRASE_FOR_reviewing
+    else:
+        verdict = PHRASE_FOR_approved
+    return PHRASE_FOR_otvet.format(
+        name_to_insert=homework_name, verdict_to_insert=verdict)
+
+HEADERS = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
 
 
 def get_homeworks(current_timestamp):
-    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+    headers = HEADERS
     payload = {'from_date': current_timestamp}
     try:
         homework_statuses = requests.get(URL, headers=headers, params=payload)
-    except Exception as error:
-        print(f'Чет не то: {error}')
+    except TypeError as error:
         logging.error(error, exc_info=True)
+        raise error('что-то не так с {homework_statuses}')
     return homework_statuses.json()
 
 
@@ -66,14 +75,14 @@ def main():
         try:
             home_work_status = get_homeworks(current_timestamp)
             for homework in reversed(home_work_status['homeworks']):
-                result = parse_homework_status(homework)
+                result = parse_homework_status(homework[1])
                 send_message(result)
             time.sleep(5 * 60)  # Опрашивать раз в пять минут
 
-        except Exception as e:
-            print(f'Бот упал с ошибкой: {e}')
-            time.sleep(5)
-            logging.error(e, exc_info=True)
+        except Exception as error:
+            print(f'Не получилось достать домашку: {error}')
+            logging.error(error, exc_info=True)
+            time.sleep(13 * 60)
 
 
 if __name__ == '__main__':
