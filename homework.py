5b7b231645f1fc85946b0e70248f2cbfa012cc2f
stas-zatushevskii <86684411+stas-zatushevskii@users.noreply.github.com>
2021-09-24 18:40:19 +0300

    a

--- homework.py
+++ homework.py
@@ -1,5 +1,4 @@
 import logging
-from logging.handlers import RotatingFileHandler
 import os
 import time
 
@@ -17,52 +16,56 @@ URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 
 bot = Bot(token=TELEGRAM_TOKEN)
 
+
 if __name__ == '__main__':
     logging.basicConfig(
         level=logging.INFO,
-        filename='main.log',
+        filename=os.path.expanduser('~/api_sp1_bot/main.log'),
         filemode='w',
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
 
-    ), os.path.expanduser('~/main.log')
+    ),
 
 
-logger = logging.getLogger(__name__)
-handler = RotatingFileHandler(
-    'my_logger.log', maxBytes=50000000, backupCount=5
-)
-
-PHRASE_FOR_rejected = 'К сожалению, в работе нашлись ошибки.'
-PHRASE_FOR_reviewing = 'Работа взята в ревью'
-PHRASE_FOR_approved = 'Ревьюеру всё понравилось, работа зачтена!'
-PHRASE_FOR_otvet = (
-    'У вас проверили работу "{name_to_insert}" !\n\n{verdict_to_insert}')
+REJECTED = 'К сожалению, в работе нашлись ошибки.'
+REVIEWING = 'Работа взята в ревью'
+PPROVED = 'Ревьюеру всё понравилось, работа зачтена!'
+ANSWER = (
+    'У вас проверили работу "{name}" !\n\n{verdict}')
+UNEXPECTED_RESPONSE = 'Неожиданный ответ от сервера'
+ERROR = 'Сервер сообщил про отказ'
 
 
 def parse_homework_status(homework):
-    homework_name = homework['homework_name']
     if homework['status'] == 'rejected':
-        verdict = PHRASE_FOR_rejected
+        verdict = REJECTED
     elif homework['status'] == 'reviewing':
-        verdict = PHRASE_FOR_reviewing
+        verdict = REVIEWING
+    elif homework['status'] == 'approved':
+        verdict = PPROVED
     else:
-        verdict = PHRASE_FOR_approved
-    return PHRASE_FOR_otvet.format(
-        name_to_insert=homework_name, verdict_to_insert=verdict)
+        verdict = UNEXPECTED_RESPONSE
+
+    return ANSWER.format(
+        name=homework['homework_name'], verdict=verdict)
 
 
 HEADERS = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
 
 
 def get_homeworks(current_timestamp):
-    headers = HEADERS
     payload = {'from_date': current_timestamp}
     try:
-        homework_statuses = requests.get(URL, headers=headers, params=payload)
-    except TypeError as error:
-        logging.error(error, exc_info=True)
-        raise error('что-то не так с {homework_statuses}')
-    return homework_statuses.json()
+        homework_statuses = requests.get(URL, headers=HEADERS, params=payload)
+    except ConnectionError as error:
+        raise error(f'Ошибка соединения, {payload}')
+    # на случай если ответ от яндекса не утешающий
+    if homework_statuses == 'error':
+        return ERROR
+    elif homework_statuses == 'code':
+        return ERROR
+    else:
+        return homework_statuses.json()
 
 
 def send_message(message):
@@ -75,13 +78,15 @@ def main():
     while True:
         try:
             home_work_status = get_homeworks(current_timestamp)
-            for homework in reversed(home_work_status['homeworks']):
-                result = parse_homework_status(homework[1])
-                send_message(result)
+            homework = reversed(home_work_status['homeworks'])
+            result = parse_homework_status(homework[1])
+            # на случай если не правильно -
+            # (не понимаю как тут обойтись без цыкла)
+            send_message(result)
             time.sleep(5 * 60)  # Опрашивать раз в пять минут
 
         except Exception as error:
-            print(f'Не получилось достать домашку: {error}')
+            print(f'что-то не получилось {error}')
             logging.error(error, exc_info=True)
             time.sleep(13 * 60)
 
